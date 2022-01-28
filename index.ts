3b7241e87b1e613f49df63cf5040950330d2244c
salah <s7baijan@hotmail.com>
2022-01-28 20:58:04 +0300

    born

---
+++ index.ts
@@ -0,0 +1,28 @@
+import { readFileSync } from "fs"
+
+export function envParser(FILE: string = ".env") {
+  const env = readFileSync(FILE, "utf-8")
+  const vars: Record<string, string> = {}
+  env
+    .split("\n")
+    .filter((e) => e !== "" && !e.includes("#") && e !== "\r")
+    .map((e) => e.replace("\r", ""))
+    .forEach((e) => {
+      const [key, value] = e.split("=") as any[]
+      if (key && value) {
+        let parsedValue: any = value
+        if (!isNaN(value)) {
+          console.log(Number(value))
+          parsedValue = Number(value)
+        } else if (typeof value === "string") {
+          parsedValue = String(value)
+        } else if (typeof value === "boolean") {
+          parsedValue = Boolean(value)
+        }
+        vars[key] = parsedValue
+      }
+    })
+  for (const key in vars) {
+    process.env[key] = vars[key]
+  }
+}
